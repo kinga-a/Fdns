3d9c714e665f657d62c4015020ef3d958a2d423f
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-06-07 15:17:13 +0800

    Update middleware.js

--- middleware.js
+++ middleware.js
@@ -1,4 +1,4 @@
-// middleware.js - 根目录中间件（空壳，配合 functions/_middleware.js 使用）
+// middleware.js - 根目录中间件（空壳，配合 edge-functions/_middleware.js 使用）
 export function middleware(context) {
     return context.next();
 }
