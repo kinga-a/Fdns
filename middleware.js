45c2cdd4127575757c0862385bce9c88f67fb91f
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-06-07 15:50:37 +0800

    Delete middleware.js

--- middleware.js
+++
@@ -1,8 +0,0 @@
-// middleware.js - 根目录中间件（空壳，配合 edge-functions/_middleware.js 使用）
-export function middleware(context) {
-    return context.next();
-}
-
-export const config = {
-    matcher: ['/:path*'],
-};
