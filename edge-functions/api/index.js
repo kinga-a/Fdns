6c12b8a044afc8056a92e7a3a965ddb67660f65d
kinga-a <56465232+kinga-a@users.noreply.github.com>
2026-06-08 21:36:27 +0800

    Create index.js

---
+++ edge-functions/api/index.js
@@ -0,0 +1,8 @@
+// edge-functions/index.js
+export async function onRequest(context) {
+    const { request, env } = context;
+    const url = new URL(request.url);
+
+    // 直接重定向到 dnsindex.html，不再拦截密码
+    return Response.redirect(url.origin + '/', 302);
+}
